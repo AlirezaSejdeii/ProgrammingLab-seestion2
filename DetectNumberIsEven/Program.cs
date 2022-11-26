9533287545b47915ebb04212d23eef1e5b44a92c
alirezasejdeii <alireza.sejdei@gmail.com>
2022-11-27 01:58:58 +0330

    Redandent

--- DetectNumberIsEven/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿Console.WriteLine("Detect number is Even or not.");
-
-int number = int.Parse(Console.ReadLine());
-
-string message = (number % 2 == 1) ? "This number is first" : "Number is last";
-
-Console.WriteLine(message);
-
